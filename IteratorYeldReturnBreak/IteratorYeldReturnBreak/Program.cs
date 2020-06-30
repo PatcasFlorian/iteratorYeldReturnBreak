4fca9e07f29ed67c5861a590cc70bdedc8befdb3
Patcas Florian <patcasf12@gmail.com>
2020-06-30 18:06:23 +0300

    Tema IteratorYeldReturn

---
+++ IteratorYeldReturnBreak/IteratorYeldReturnBreak/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace IteratorYeldReturnBreak
+{
+
+    class Iterator
+    {
+
+        public static IEnumerable<int> GetMyMaxValue()
+        {
+           
+            int maxValue= Int32.MaxValue;
+            
+            for (int items = 0;items <= maxValue;items++)
+            {
+                if (items > maxValue - 3)
+                {
+                 yield break;
+                }
+
+                yield return items;
+            }
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> myitems = GetMyMaxValue();
+            // Display the elements return from iteration 
+            foreach (var i in GetMyMaxValue())
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+
+        private static IEnumerable<string> GetMyMaxValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NewIteratorYeld/NewIteratorYeld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewIteratorYeld
+{
+    class Program
+    {
+        public static IEnumerable<int> GetMyMaxValue()
+        {
+
+            int maxValue = Int32.MaxValue;
+
+            for (int items = 0; items <= maxValue; items++)
+            {
+                if (items == maxValue / 50000)
+                {
+                    yield break;
+                }
+
+                yield return items;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            // Display the elements return from iteration 
+            foreach (var i in GetMyMaxValue())
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
